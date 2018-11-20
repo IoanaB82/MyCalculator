8b0b11163b75dcd0a6eaf8284d37800a73121aa2
ioana <ioana.valentir@gmail.com>
2018-11-20 13:29:46 +0100

    added class variables

---
+++ MyCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MyCalculator{
+  private static double firstNumber = 0;
+  private static double secondNumber = 0;
+  private static double answer = 0;
+  private static String operator = "-1";
+
+  
+
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+        String run_again = new String();
+            while(run_again != "N") {
+        System.out.println('\n'+"Which operation do you want to run? Type A for Addition, S for Substraction, M for Multiplication or D for Division");
+        String letter = scan.next();
+  }
+
+}
